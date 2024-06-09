476cce2f84220c304745d28d49fe8163210b9104
Pavani <vemasani.pavani@gmail.com>
2024-06-09 22:51:02 +0530

    create vault provider and get credentails from vault

---
+++ module/app/data.tf
@@ -0,0 +1,3 @@
+data "vault_generic_secret" "rundeck_auth" {
+  path = "common/ssh"
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "vault" {
+  address = "https://54.226.227.17:8200"
+  token = "hvs.RTDXtStxH3G1DIGzKWmDmojA"
+  skip_tls_verify = true
+}
