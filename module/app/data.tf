3456e31c1059113193c02241810c306a4cb7fe38
Pavani <vemasani.pavani@gmail.com>
2024-06-09 23:30:54 +0530

    get secrets and store into a file

--- module/app/data.tf
+++ module/app/data.tf
@@ -2,6 +2,6 @@ data "vault_generic_secret" "rundeck_auth" {
   path = "common/ssh"
 }
 resource "local_file" "foo" {
-  content  = data.vault_generic_secret.rundeck_auth
+  content  = data.vault_generic_secret.rundeck_auth.data_json
   filename = "/tmp/secrets"
 }
